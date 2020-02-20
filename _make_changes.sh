0eb1ba6d0a492edf5c40feee21fde57c7031c69d
elangeloscuro1 <angeltapia07084759@gmail.com>
2020-02-20 04:33:42 -0800

    2020-02-20 04:33:42 =>

--- _make_changes.sh
+++ _make_changes.sh
@@ -1,6 +1,57 @@
-git add .
-#git commit -m "`date '+%Y-%m-%d %H:%M:%S'`"
-read -p "Enter message: " userInput
-git commit -m "`date '+%Y-%m-%d %H:%M:%S'` => $userInput"
-git push
+#!/bin/bash#!/bin/bash
+function toLowerCase() #USE:=$(toLowerCase "$str")
+{
+	echo "$1" | tr '[:upper:]' '[:lower:]'
+}
+
+########### check for files renaming #### START
+git config core.ignorecase false              #
+hasChanged="$(git status --porcelain)"        #
+gitFilesUnTracked=$(git ls-files -o)          # get untracked files only
+git config core.ignorecase true               #
+gitFilesTracked=$(git ls-files)               #
+SAVEIFS=$IFS                                  # Save current IFS
+IFS=$'\n'                                     # Change IFS to new line
+gitFilesTracked=($gitFilesTracked)            # split to array $names
+gitFilesUnTracked=($gitFilesUnTracked)        # split to array $names
+IFS=$SAVEIFS                                  # Restore IFS
+########### check for files renaming ###### END
+
+date="`date '+%b %d, %Y; %H:%M:%S; %Z'`"
+
+if [[ -n "$hasChanged" ]]
+then
+	echo "There are changes!";
+	read -p "Enter a commit message: " userInput
+	
+	for fileTracked in "${gitFilesTracked[@]}"
+	do
+		echo "$fileTracked" ###
+		for fileUnTracked in "${gitFilesUnTracked[@]}"
+		do
+			if [[ "$(toLowerCase "$fileTracked")" == "$(toLowerCase "$fileUnTracked")" ]]
+			then
+				echo "$fileTracked ==[${#gitFilesTracked[@]}, ${#gitFilesUnTracked[@]}]== $fileUnTracked"
+				#gitFilesUnTracked=( ${gitFilesUnTracked[@]/"$fileUnTracked"} )
+				[[ $(git rm --cached "$fileTracked") ]] || [[ $(git rm -f --cached "$fileTracked") ]]
+				#git add "$fileUnTracked"
+				#read -p "Press [Enter]"
+				break
+			fi
+		done
+	done 
+	#read -p "${#gitFilesTracked[@]} ========== ${#gitFilesUnTracked[@]}" #####-----------
+	#git ls-files ###
+	### Checking for files renaming (case sensitive) ENDS
+	
+	echo $date>"date"
+	git add .
+	git commit -m "`date '+%Y-%m-%d %H:%M:%S'` => $userInput"
+	git push
+	echo
+else
+	echo "There are NO changes!";
+fi
+
 read -p "Press [Enter] to continue..."
+
